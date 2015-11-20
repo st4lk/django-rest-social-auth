24529e0b9dc45f5c4f5bb0b466579e91bb27d183
James Keys <skolsuper@gmail.com>
2015-11-20 14:35:31 +0700

    Factor out unnecessary repetition

--- rest_social_auth/views.py
+++ rest_social_auth/views.py
@@ -94,8 +94,7 @@ class BaseSocialAuthView(GenericAPIView):
         self.set_input_data(request, input_data)
         decorate_request(request, input_data['provider'])
         serializer_in = self.get_serializer_in(data=input_data)
-        if (isinstance(serializer_in, OAuth1InputSerializer) and
-                request.backend.OAUTH_TOKEN_PARAMETER_NAME not in input_data):
+        if self.oauth_v1() and request.backend.OAUTH_TOKEN_PARAMETER_NAME not in input_data:
             # If this is oauth1 and first stage (1st is get request_token, 2nd is get access_token)
             request_token = parse_qs(request.backend.set_unauthorized_token())
             return Response(request_token)
