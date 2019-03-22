648d1c44f463a4896de26a061df6e104bf553bcc
Alexey Evseev <myhappydo@gmail.com>
2019-03-23 01:21:26 +0300

    Enable knox tests and split tests (#83)
    Enable knox tests and split tests

---
+++ tests/base.py
@@ -0,0 +1,60 @@
+import json
+
+from httpretty import HTTPretty
+from social_core.backends.utils import load_backends
+from social_core.tests.backends.test_facebook import FacebookOAuth2Test
+from social_core.tests.backends.test_twitter import TwitterOAuth1Test
+from social_core.utils import module_member
+
+from rest_social_auth import views
+
+
+# don't run third party tests
+for attr in (attr for attr in dir(FacebookOAuth2Test) if attr.startswith('test_')):
+    delattr(FacebookOAuth2Test, attr)
+for attr in (attr for attr in dir(TwitterOAuth1Test) if attr.startswith('test_')):
+    delattr(TwitterOAuth1Test, attr)
+
+
+class RestSocialMixin(object):
+    def setUp(self):
+        HTTPretty.enable()
+        Backend = module_member(self.backend_path)
+        self.strategy = views.load_strategy()
+        self.backend = Backend(self.strategy, redirect_uri=self.complete_url)
+        self.name = self.backend.name.upper().replace('-', '_')
+        self.complete_url = self.strategy.build_absolute_uri(
+            self.raw_complete_url.format(self.backend.name)
+        )
+        backends = (self.backend_path, )
+        load_backends(backends, force_load=True)
+
+        user_data_body = json.loads(self.user_data_body)
+        self.email = 'example@mail.com'
+        user_data_body['email'] = self.email
+        self.user_data_body = json.dumps(user_data_body)
+
+        self.do_rest_login()
+
+    def tearDown(self):
+        HTTPretty.disable()
+        HTTPretty.reset()
+        self.backend = None
+        self.strategy = None
+        self.name = None
+        self.complete_url = None
+
+
+class BaseFacebookAPITestCase(RestSocialMixin, FacebookOAuth2Test):
+
+    def do_rest_login(self):
+        start_url = self.backend.start().url
+        self.auth_handlers(start_url)
+
+
+class BaseTwitterApiTestCase(RestSocialMixin, TwitterOAuth1Test):
+
+    def do_rest_login(self):
+        self.request_token_handler()
+        start_url = self.backend.start().url
+        self.auth_handlers(start_url)
---
+++ tests/test_token.py
@@ -0,0 +1,99 @@
+import json
+
+from django.test import modify_settings
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from social_core.utils import parse_qs
+
+from .base import BaseFacebookAPITestCase, BaseTwitterApiTestCase
+
+
+token_modify_settings = dict(
+    INSTALLED_APPS={
+        'remove': [
+            'django.contrib.sessions',
+            'knox',
+        ]
+    },
+    MIDDLEWARE_CLASSES={
+        'remove': [
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+        ],
+    }
+)
+
+
+class TestSocialAuth1Token(APITestCase, BaseTwitterApiTestCase):
+
+    def test_login_social_oauth1_token(self):
+        """
+        Currently oauth1 works only if session is enabled.
+        Probably it is possible to make it work without session, but
+        it will be needed to change the logic in python-social-auth.
+        """
+        resp = self.client.post(
+            reverse('login_social_token_user'), data={'provider': 'twitter'})
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data, parse_qs(self.request_token_body))
+        resp = self.client.post(reverse('login_social_token_user'), data={
+            'provider': 'twitter',
+            'oauth_token': 'foobar',
+            'oauth_verifier': 'overifier'
+        })
+        self.assertEqual(resp.status_code, 200)
+
+
+class TestSocialAuth2Token(APITestCase, BaseFacebookAPITestCase):
+
+    @modify_settings(**token_modify_settings)
+    def _check_login_social_token_user(self, url, data):
+        resp = self.client.post(url, data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['email'], self.email)
+        # check token exists
+        token = Token.objects.get(key=resp.data['token'])
+        # check user is created
+        self.assertEqual(token.user.email, self.email)
+
+    @modify_settings(**token_modify_settings)
+    def _check_login_social_token_only(self, url, data):
+        resp = self.client.post(url, data)
+        self.assertEqual(resp.status_code, 200)
+        # check token exists
+        token = Token.objects.get(key=resp.data['token'])
+        # check user is created
+        self.assertEqual(token.user.email, self.email)
+
+    def test_login_social_token_user(self):
+        self._check_login_social_token_user(
+            reverse('login_social_token_user'),
+            {'provider': 'facebook', 'code': '3D52VoM1uiw94a1ETnGvYlCw'})
+
+    def test_login_social_token_user_provider_in_url(self):
+        self._check_login_social_token_user(
+            reverse('login_social_token_user', kwargs={'provider': 'facebook'}),
+            {'code': '3D52VoM1uiw94a1ETnGvYlCw'})
+
+    def test_login_social_token_only(self):
+        self._check_login_social_token_only(
+            reverse('login_social_token'),
+            data={'provider': 'facebook', 'code': '3D52VoM1uiw94a1ETnGvYlCw'})
+
+    def test_login_social_token_only_provider_in_url(self):
+        self._check_login_social_token_only(
+            reverse('login_social_token', kwargs={'provider': 'facebook'}),
+            data={'code': '3D52VoM1uiw94a1ETnGvYlCw'})
+
+    def test_user_login_with_no_email(self):
+        user_data_body = json.loads(self.user_data_body)
+        user_data_body['email'] = ''
+        self.user_data_body = json.dumps(user_data_body)
+        self.do_rest_login()
+        resp = self.client.post(
+            reverse('login_social_token'), data={'provider': 'facebook', 'code': '3D52VoM1uiw94a1ETnGvYlCw'}
+        )
+        self.assertEqual(resp.status_code, 400)
+        self.assertIn('error', resp.data)
