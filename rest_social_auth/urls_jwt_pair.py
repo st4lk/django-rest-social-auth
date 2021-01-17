60da83ccf046ee2504c51c003dd4d13430743a07
Alexey Evseev <myhappydo@gmail.com>
2021-01-17 20:41:10 +0300

    Refactor code to replace url() with re_path() (#140)
    * Refactor code to replace url() with re_path()
    
    * url() replaced with path()
    
    Co-authored-by: Dipendra <dipendrarajpanta1@gmail.com>
    Co-authored-by: Dipendra Raj Panta <49410574+Mr-DRP@users.noreply.github.com>

--- rest_social_auth/urls_jwt_pair.py
+++ rest_social_auth/urls_jwt_pair.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 
 urlpatterns = (
     # returns token only
-    url(r'^social/jwt-pair/(?:(?P<provider>[a-zA-Z0-9_-]+)/?)?$',
-        views.SocialJWTPairOnlyAuthView.as_view(),
-        name='login_social_jwt_pair'),
+    re_path(r'^social/jwt-pair/(?:(?P<provider>[a-zA-Z0-9_-]+)/?)?$',
+            views.SocialJWTPairOnlyAuthView.as_view(),
+            name='login_social_jwt_pair'),
     # returns token + user_data
-    url(r'^social/jwt-pair-user/(?:(?P<provider>[a-zA-Z0-9_-]+)/?)?$',
-        views.SocialJWTPairUserAuthView.as_view(),
-        name='login_social_jwt_pair_user'),
+    re_path(r'^social/jwt-pair-user/(?:(?P<provider>[a-zA-Z0-9_-]+)/?)?$',
+            views.SocialJWTPairUserAuthView.as_view(),
+            name='login_social_jwt_pair_user'),
 )
--- rest_social_auth/urls_session.py
+++ rest_social_auth/urls_session.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 
 urlpatterns = (
-    url(r'^social/session/(?:(?P<provider>[a-zA-Z0-9_-]+)/?)?$',
-        views.SocialSessionAuthView.as_view(),
-        name='login_social_session'),)
+    re_path(r'^social/session/(?:(?P<provider>[a-zA-Z0-9_-]+)/?)?$',
+            views.SocialSessionAuthView.as_view(),
+            name='login_social_session'),)
