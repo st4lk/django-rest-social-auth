90cba05181303c4735edbc058fbf599a3cf7033d
Alexey Evseev <myhappydo@gmail.com>
2022-11-19 00:22:34 +0300

    Fix tests

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,7 @@ PROJECT_PATH=/django_rest_social_auth
 # create virtualenv if it doesn't exist
 python -m venv /${PROJECT_PATH}/venv
 . ${PROJECT_PATH}/venv/bin/activate
+python -m pip install -U pip
 
 export PYENV_ROOT="${PROJECT_PATH}/.pyenv"
 export PATH="${PROJECT_PATH}/.pyenv/bin:${PATH}"
